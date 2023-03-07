546f75467085c33a6e562ab097a218fc6e042b8b
Yefry Sanchez <yefrymsp@gmail.com>
2023-03-06 19:12:26 -0500

    setting up stripe

---
+++ src/api/stripe.js
@@ -0,0 +1,10 @@
+import Stripe from "stripe";
+
+
+const stripe = new Stripe(import.meta.env.VITE_REACT_APP_STRIPE_SECRET )
+
+
+
+
+
+
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 export const Checkout = () => {
+
+const [activeStep, setActiveStep] = useState(0)
+const cart = useSelector((state ) => state.cart.cart)
+const isFirstStep = activeStep === 0
+const isSecondStep = activeStep === 1
+
+const handleFormSubmit =() => {
+  setActiveStep(activeStep + 1)
+}
+
+async function makePayment (value) {
+
+}
+
+
   return (
-    <div className='pt-[80px]'>Checkout</div>
+    <div className='pt-[80px] w-4/5 mx-auto'>
+      Checkout
+    </div>
   )
 }
